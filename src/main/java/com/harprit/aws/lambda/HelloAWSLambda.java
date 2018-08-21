88b7752fb3f3b9fb021c76cf368d21be7a67d95b
Harprit Singh <harprits10@gmail.com>
2018-08-22 07:18:41 +0800

    first commit

---
+++ src/main/java/com/harprit/aws/lambda/HelloAWSLambda.java
@@ -0,0 +1,9 @@
+package com.harprit.aws.lambda;
+
+public class HelloAWSLambda {
+
+	public void handler(String input) {
+		String message = String.format("Hello %s", input);
+		System.out.println(message);
+	}
+}
