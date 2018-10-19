56d4fb329ade238b730213186084595cb10dfc57
Harprit Singh <harprits10@gmail.com>
2018-10-20 00:40:40 +0800

    cleaned up

--- src/main/java/com/harprit/aws/lambda/HelloAWSLambda.java
+++
@@ -1,9 +0,0 @@
-package com.harprit.aws.lambda;
-
-public class HelloAWSLambda {
-
-	public void handler(String input) {
-		String message = String.format("Hello %s", input);
-		System.out.println(message);
-	}
-}
